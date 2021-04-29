909d2f94a096b1bd751d9f9bb0576c60a60cbdfc
Calowsky <78949858+Calowsky@users.noreply.github.com>
2021-04-29 19:14:23 -0400

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,8 +1,8 @@
 var http = require('http');
 var port = process.env.PORT || 3000;
 
-const axios = require('axios');
-const url = 'https://www.premierleague.com/stats/top/players/goals?se=-1&cl=-1&iso=-1&po=-1?se=-1';
+//const axios = require('axios');
+//const url = 'https://www.premierleague.com/stats/top/players/goals?se=-1&cl=-1&iso=-1&po=-1?se=-1';
 
 
 http.createServer(function (req, res) {
