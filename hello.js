466216eaefe100eb37b1e09b39eb271740a3b13f
Calowsky <78949858+Calowsky@users.noreply.github.com>
2021-05-01 18:57:28 -0400

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var port = process.env.PORT || 3000;
+var qs = require('querystring');
 
 //const axios = require('axios');
 //const url = 'https://www.premierleague.com/stats/top/players/goals?se=-1&cl=-1&iso=-1&po=-1?se=-1';
@@ -8,6 +9,12 @@ var port = process.env.PORT || 3000;
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write("<h2>Hello World</h2>");    
+  
+    if (req.url == "/")  {  file = 'index.html';  fs.readFile(file, function(err, txt) {  res.writeHead(200, {'Content-Type': 'text/html'});  res.write("This is the home page<br>");          res.write(txt);          res.end();  });  }
+  
+  
+  
+  
   res.end();
 }).listen(port);
 
