df227b4c60e554e4cf624f78852eb059b5831792
Calowsky <78949858+Calowsky@users.noreply.github.com>
2021-05-01 19:00:56 -0400

    Update hello.js

--- hello.js
+++ hello.js
@@ -7,14 +7,38 @@ var qs = require('querystring');
 
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write("<h2>Hello World</h2>");    
+
+    if (req.url == "/")  {
+      file = 'formpage.html';
+      fs.readFile(file, function(err, txt) {
+        res.writeHead(200, {'Content-Type': 'text/html'});  
+        
+        res.write("This is the home page<br>");          
+        res.write(txt);          
+        res.end();  }); 
+    }
   
-    if (req.url == "/")  {  file = 'index.html';  fs.readFile(file, function(err, txt) {  res.writeHead(200, {'Content-Type': 'text/html'});  res.write("This is the home page<br>");          res.write(txt);          res.end();  });  }
+  else if (req.url == "/process")  { 
+    res.writeHead(200, {'Content-Type':'text/html'}); 
+    res.write ("Process the form<br>"); 
+    pdata = ""; req.on('data', data => {           
+      pdata += data.toString();         });// when complete POST data is received
+    
+    req.on('end', () => {
+      pdata = qs.parse(pdata);
+      res.write ("The name is: "+ pdata['the_name']);
+      res.end();
+    });  }  
+  
+  else   {
+    res.writeHead(200, {'Content-Type':'text/html'});  
+    res.write ("Unknown page request"); 
+    res.end();  
+  }
   
   
   
   
-  res.end();
+  
 }).listen(port);
 
